bb7ead18b1ba782a42f0bceaa30d56b6e0962353
Aldriech Villamor <81332170+aldriechV@users.noreply.github.com>
2026-07-27 13:45:19 -0700

    Backend

---
+++ backend/routes/cardRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+const cardController = require("../controllers/cardController");
+
+router.get("/", cardController.getCards);
+router.get("/:id", cardController.getCardById);
+router.post("/", cardController.createCard);
+router.put("/:id", cardController.updateCard);
+router.delete("/:id", cardController.deleteCard);
+
+module.exports = router;
